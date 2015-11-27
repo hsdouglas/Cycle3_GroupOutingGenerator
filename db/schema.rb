920895e28ddd3ee58c540eb3779a9d5471de8bcb
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-11-27 16:59:25 -0500

    added field to identify groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127214542) do
+ActiveRecord::Schema.define(version: 20151127215847) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151127214542) do
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "tickets", force: :cascade do |t|
